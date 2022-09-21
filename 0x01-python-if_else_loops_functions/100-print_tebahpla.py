5888f2d9819e5cccf4daaae19a5c46bfaaf3294b
akorede <emiolasaheed1@gmail.com>
2022-09-21 01:14:21 +0000

    alpha

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+i = 122
+while i >= 97:
+    if i % 2 != 0:
+        diff = 32
+    else:
+        diff = 0
+    print("{}".format(chr(i - diff)), end="")
+    i = i - 1
+        
---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * insert_node - inserts node
+ * @head: head
+ * @number: number
+ * Return: pointer
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+}
