ba348277cb26e313c76ec7a0e4ac92f47d40bd41
akorede <emiolasaheed1@gmail.com>
2022-09-22 14:16:17 +0000

    hidden

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import hidden_4
-str = dir(hidden_4)
-str = str[-3:]
-for elem in str:
-    print(elem)
+if __name__ == "__main__":
+    import hidden 4
+    str = dir(hidden_4)
+    str = str[-3:]
+    for elem in str:
+        print(elem)
