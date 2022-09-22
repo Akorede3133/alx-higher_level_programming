4c9134e87c575a8ea96face19c8e1434a403efb7
akorede <emiolasaheed1@gmail.com>
2022-09-22 13:38:12 +0000

    infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,10 +2,10 @@
 if __name__ == "__main__":
     import sys
     arglen = len(sys.argv)
-    res = 0;
+    res = 0
     if arglen == 1:
         res += 0
-    else:   
+    else:
         for elemIndex in range(1, arglen):
             res += int(sys.argv[elemIndex])
     print(res)
