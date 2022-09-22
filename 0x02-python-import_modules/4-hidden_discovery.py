c6cb3490d908fe3f9436a293a880098cd39fcc1b
akorede <emiolasaheed1@gmail.com>
2022-09-22 14:30:44 +0000

    hidden

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    str = dir(hidden_4)
-    str = str[-3:]
-    for elem in str:
+    string = []
+    dir_str = dir(hidden_4)
+    for elem in dir_str:
+        if elem[0] != '_':
+            string.append(elem)
+    for elem in string:
         print(elem)
