4cfe4be4cf21fa0e9b0156c880e11d89eb69456b
akorede <emiolasaheed1@gmail.com>
2022-09-26 07:16:39 +0000

    tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -10,9 +10,3 @@ def add_tuple(tuple_a=(), tuple_b=()):
     for i in range(0, 2):
         res += (tuple_a[i] + tuple_b[i],)
     return(res)
-tuple_a = (1, 89)
-tuple_b = (88, 11)
-new_tuple = add_tuple(tuple_a, tuple_b)
-print(new_tuple)
-print(add_tuple(tuple_a, (1, )))
-print(add_tuple(tuple_a, ()))
