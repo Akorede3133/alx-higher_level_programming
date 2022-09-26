eaa2cbf14537a6720bf8448cedb7daf3e79f9f0f
akorede <emiolasaheed1@gmail.com>
2022-09-26 07:15:38 +0000

    matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,8 +4,9 @@ def print_matrix_integer(matrix=[[]]):
     if height == 1:
         print(end="")
     for h in range(0, height):
-        for w in range(0, len(matrix[h])):
-            if w < len(matrix[h]) - 1:
+        width = len(matrix[h])
+        for w in range(0, width):
+            if w < width - 1:
                 print("{:d}".format(int(matrix[h][w])), end=" ")
             else:
                 print("{:d}".format(int(matrix[h][w])), end="")
