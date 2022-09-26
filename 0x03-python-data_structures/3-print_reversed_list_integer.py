b62a39fbd6413758fe93b386992a72449a79603d
akorede <emiolasaheed1@gmail.com>
2022-09-26 07:13:03 +0000

    matrix

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,5 +3,3 @@ def print_reversed_list_integer(my_list=[]):
     list_len = len(my_list)
     for elemIndex in range(list_len, 0, -1):
         print("{:d}".format(int(my_list[elemIndex - 1])))
-my_list = [1, 2, 3, 4, 5]
-print_reversed_list_integer(my_list)
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,7 +6,7 @@ def print_matrix_integer(matrix=[[]]):
     for h in range(0, height):
         for w in range(0, len(matrix[h])):
             if w < len(matrix[h]) - 1:
-                print("{}".format(matrix[h][w]), end=" ")
+                print("{:d}".format(int(matrix[h][w])), end=" ")
             else:
-                print("{}".format(matrix[h][w]), end="")
+                print("{:d}".format(int(matrix[h][w])), end="")
         print()
