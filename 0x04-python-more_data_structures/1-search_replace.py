0651763adc3a337073f7d47bd328ed77ed729d7e
akorede <emiolasaheed1@gmail.com>
2022-09-28 14:33:52 +0000

    replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     length = len(my_list)
+    new_list = my_list[:]
     for i in range(0, length):
-        if my_list[i] == search:
-            my_list[i] = replace
-    return my_list
+        if new_list[i] == search:
+            new_list[i] = replace
+    return new_list
+my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
+new_list = search_replace(my_list, 2, 89)
+
+print(new_list)
+print(my_list)
