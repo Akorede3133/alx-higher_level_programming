a756e777632de071ea2f589ac5d3954cb58880a3
akorede <emiolasaheed1@gmail.com>
2022-09-28 15:57:12 +0000

    del

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-print_sorted_dictionary = \
-            __import__('6-print_sorted_dictionary').print_sorted_dictionary
-
 def simple_delete(a_dictionary, key=""):
-    if key  not in a_dictionary.keys():
+    if key  not in a_dictionary:
         return
     else:
         del a_dictionary[key]
