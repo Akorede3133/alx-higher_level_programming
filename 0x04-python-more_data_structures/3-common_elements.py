2d8e198162ebd7ead49788731268ad63b7c666f9
akorede <emiolasaheed1@gmail.com>
2022-09-28 14:59:03 +0000

    set

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    new_set = set()
-    for elem in set_1:
-        if elem in set_2:
-            new_set = {elem}
-    return new_set
+    new_set = []
+    list_from_set = list(set_1)
+    list_from_set.extend(list(set_2))
+    for elem in list_from_set:
+        if elem in set_1 and elem in set_2:
+            new_set.append(elem)
+    return set(new_set)
