a2b82571195be512fac51db58c5e8d8ac1dc7727
akorede <emiolasaheed1@gmail.com>
2022-10-10 20:55:43 +0000

    exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     length = 0
-    try:
-        for i in range(x):
+    for i in range(x):
+        try:
             print(my_list[i], end="")
-        print()
-        return i + 1
-    except (IndexError, UnboundLocalError):
-        print()
-        for elem in my_list:
-            length += 1
-        return length
+            length = length + 1
+        except (IndexError, UnboundLocalError):
+            break
+    print()
+    return(length)
