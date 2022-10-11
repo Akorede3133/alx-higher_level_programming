0219934726c1ca8ed26d69fb5cb1cd75541852c7
akorede <emiolasaheed1@gmail.com>
2022-10-11 21:13:37 +0000

    cls

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""A square class with stuff"""
+
+
+class Square:
+    """A square class attributes are as follows"""
+    def __init__(self):
+        """Defining the attributes of each instance of the class"""
+        self.__size = 4
