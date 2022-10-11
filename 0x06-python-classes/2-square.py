836032f16c01f81fd657c7f72bfa26e20e8fe1d8
akorede <emiolasaheed1@gmail.com>
2022-10-11 21:36:43 +0000

    error

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Creating a square class"""
+
+
+class Square:
+    """giving atrributes to the class"""
+    def __init__(self, size=0):
+        """instantiator for each instances"""
+        if type(size) != int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
