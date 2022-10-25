d405b61f313110ff7bb59719d5984a6b6e041405
akorede <emiolasaheed1@gmail.com>
2022-10-25 22:51:42 +0000

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """a func to read from a file"""
-    with open(filename, encoding="utf-8") as my_file:
+    with open(filename, mode='r', encoding="utf-8") as my_file:
         print(my_file.read())
