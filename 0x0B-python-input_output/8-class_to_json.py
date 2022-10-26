eb2dd7b1d5772e5f65cc8af51e6827ea270465de
akorede <emiolasaheed1@gmail.com>
2022-10-26 13:46:08 +0000

    dict

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" a function that returns the dict desc with simple data struct"""
+
+
+def class_to_json(obj):
+    """ a function that returns the dict desc with simple data struct"""
+    return (obj.__dict__)
