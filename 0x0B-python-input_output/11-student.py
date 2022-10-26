967412fc651834550ac8b8cc7f423b9d8e70f482
akorede <emiolasaheed1@gmail.com>
2022-10-26 14:46:52 +0000

    dict

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -26,4 +26,4 @@ class Student:
     def reload_from_json(self, json):
         """ replaces all attributes of the Student instance"""
         for key, value in json.items():
-            self.__init__(key, value)
+            self.init(key, value)
