7d5a4aca0e75522ecc5162d661331159f5ce5284
akorede <emiolasaheed1@gmail.com>
2022-10-27 06:09:11 +0000

    pascal

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" returns a list of lists of integers reping pascal"""
+
+
+def pascal_triangle(n):
+    """ returns a list of lists of integers reping pascal"""
+    parent = [[1], [1, 1]]
+    for i in range(0, n - 2):
+        child = []
+        temp = parent[i + 1]
+        for j in range(0, len(temp)):
+            if j == 0:
+                value = 1
+            else:
+                value = temp[j] + temp[j - 1]
+            child.append(value)
+        child.append(1)
+        parent.append(child)
+    return (parent)
