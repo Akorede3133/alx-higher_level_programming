3c39454cb694702fa16c85c0a109b34fb4d325fa
akorede <emiolasaheed1@gmail.com>
2022-11-05 03:32:59 +0000

    almost a circle

--- 0x0C-python-almost_a_circle/tests/test1.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-import unittest
-"""A class for testing"""
-
-
-class TestSomething(unittest.TestCase):
-    """Test something"""
-    def test_smtn(self):
-        self.assertEqual(2, 3)
